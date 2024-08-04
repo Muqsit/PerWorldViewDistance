1e3d3bfee2947fb5b2de8b3588c676f6112dac79
Muqsit <hackhack05@gmail.com>
2024-08-04 12:20:58 +0000

    Add support for PM5, drop support for PM4 (address #2)

--- src/muqsit/perworldviewdistance/Loader.php
+++ src/muqsit/perworldviewdistance/Loader.php
@@ -12,11 +12,14 @@ final class Loader extends PluginBase{
 	private PerWorldViewDistanceConfig $view_distance_config;
 	private ?PlayerManager $player_manager = null;
 
-	protected function onEnable() : void{
+	protected function onLoad() : void{
 		$this->view_distance_config = new PerWorldViewDistanceConfig((int) $this->getConfig()->get("default-view-distance"));
 		foreach($this->getConfig()->get("view-distances") as $world => $view_distance){
 			$this->view_distance_config->setViewDistance($world, $view_distance);
 		}
+	}
+
+	protected function onEnable() : void{
 		if(!$this->view_distance_config->isUnnecessary()){
 			$this->player_manager = new PlayerManager($this);
 		}
